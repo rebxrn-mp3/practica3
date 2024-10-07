7e75e25a33d2cf1c00330331dee1d3d67fa32871
rebxrn-mp3 <tfeda2005@icloud.com>
2024-10-07 16:22:17 +0500

    Update Program.cs

--- pr1609/pr1609/Program.cs
+++ pr1609/pr1609/Program.cs
@@ -9,14 +9,7 @@ namespace pr1609
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите первое слагаемое:");
-                int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите второе слагаемое:");
-                int y = int.Parse(Console.ReadLine());
-            Console.WriteLine("Сумма чисел равна: " + (x + y));
-            Console.ReadKey();
-                
+        {      
         }
     }
 }
