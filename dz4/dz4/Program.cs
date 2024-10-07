5cc6f14b5da124be3185eba0985c65c6288ea377
rebxrn-mp3 <tfeda2005@icloud.com>
2024-10-07 16:47:01 +0500

    3

---
+++ dz4/dz4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число х: ");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = myFunc(x);
+            Console.WriteLine($"f(x)={y}");
+            Console.ReadKey();
+        }
+
+        static double myFunc(double x)
+        {
+            return (2*Math.Cos(1 / (Math.Pow(x, 2) +1)) / (Math.Pow(x, 3) + 1));
+        }
+    }
+}
