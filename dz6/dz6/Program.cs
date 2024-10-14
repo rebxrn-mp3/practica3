b18f823af2a32f8c407b06c474d9fe4d0440931d
rebxrn-mp3 <tfeda2005@icloud.com>
2024-10-14 16:41:20 +0500

    DZ6

---
+++ dz6/dz6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст на русском языке: ");
+
+            var text = Console.ReadLine();
+
+            Console.WriteLine("Ваш текст транслитом: ");
+            Console.WriteLine(TransliteTranslate(text));
+
+            Console.ReadKey();
+        }
+
+        static string TransliteTranslate(String s)
+        {
+            return s
+                .ToUpper()
+                .Replace("А", "A")
+                .Replace("Б", "B")
+                .Replace("В", "V")
+                .Replace("Г", "G")
+                .Replace("Д", "D")
+                .Replace("Е", "E")
+                .Replace("Ё", "E")
+                .Replace("Ж", "ZH")
+                .Replace("З", "Z")
+                .Replace("И", "I")
+                .Replace("Й", "I")
+                .Replace("К", "K")
+                .Replace("Л", "L")
+                .Replace("М", "M")
+                .Replace("Н", "N")
+                .Replace("О", "O")
+                .Replace("П", "P")
+                .Replace("Р", "R")
+                .Replace("С", "S")
+                .Replace("Т", "T")
+                .Replace("У", "U")
+                .Replace("Ф", "F")
+                .Replace("Х", "KH")
+                .Replace("Ц", "TS")
+                .Replace("Ч", "CH")
+                .Replace("Ш", "SH")
+                .Replace("Щ", "SHCH")
+                .Replace("Ъ", "IE")
+                .Replace("Ы", "Y")
+                .Replace("Ь", "")
+                .Replace("Э", "E")
+                .Replace("Ю", "IU")
+                .Replace("Я", "IA");
+
+        }
+
+    }
+}
