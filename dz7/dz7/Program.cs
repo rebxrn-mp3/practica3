ae22a5fe6168c86be3b494efc761e7d82cf82e47
rebxrn-mp3 <tfeda2005@icloud.com>
2024-11-11 14:57:21 +0500

    3

--- dz7/dz7/Program.cs
+++ dz7/dz7/Program.cs
@@ -26,11 +26,20 @@ namespace dz7
             else
                 Console.WriteLine("Точка не принадлежит указанной области ");
 
-
             Console.ReadKey();
 
+            Console.WriteLine("Введите позицию белой пешки");
+            var whitePawnPosition = Console.ReadLine();
 
+            int whitePawnRow, whitePawnColumn;
+
+            DecodePosition(whitePawnPosition, out whitePawnRow, out whitePawnColumn);
+            Console.WriteLine($"({whitePawnRow}; {whitePawnColumn})");
+
+            Console.ReadKey();
         }
+
+        
         static int GetNumber (string numbername)
         {
             Console.WriteLine($"Введите число {numbername}: ");
@@ -45,5 +54,10 @@ namespace dz7
         {
             return (x>=2 && y>=0) || (x>=1 && y<=-1);
         }
-    }
+        static void DecodePosition(string position, out int x, out int y)
+        {
+        x = int.Parse(position[1].ToString());
+        y = (int)position[0] - 0x60;
+        }
+}
 }
